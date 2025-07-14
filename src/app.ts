60d1139d91085c2771aa59d15456353b89ed0efe
Ołówek <proallone14@gmail.com>
2025-07-14 21:04:32 +0200

    add morgan logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import morgan from 'morgan';
 import { errorHandler } from "./middlewares/errorHandler.js";
 import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan('common'))
 
 app.use('/users', userRoutes)
 
