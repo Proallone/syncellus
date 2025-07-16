874a95390e3acaed1b0b0593d956c8753e7df6ca
proallone <proallone14@gmail.com>
2025-07-16 08:35:55 +0200

    add triggers to users table

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,17 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import {  User } from "../models/user.js";
 import db from "../database/database.js";
-import { AppError } from "../middlewares/errorHandler.js";
 
 export const createUser = (req: Request, res: Response, next: NextFunction) => {
     try {
         const { name, surname, email } = req.body;
-
-        const q = db.prepare("INSERT INTO USERS (name, surname, email) VALUES (?, ?, ?) RETURNING *;");
-        const r = q.run(name, surname, email);
-        const newUser: User = { id: Date.now(), name, surname, email };
-
-        res.status(201).json(r);
+        const stmt = db.prepare("INSERT INTO USERS (name, surname, email) VALUES (?, ?, ?) RETURNING *;");
+        const stmtResult = stmt.run(name, surname, email);
+        res.status(201).json(stmtResult);
     } catch (error) {
         next(error);
     }
@@ -28,4 +23,4 @@ export const getUserById = (req: Request, res: Response, next: NextFunction) => 
     if(!user) res.status(404).send({message: `User with ID ${id} not found!`});
 
     return res.json(user);
-}
+};
--- src/database/database.ts
+++ src/database/database.ts
@@ -7,8 +7,27 @@ const init = `
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     surname TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL
-  ) STRICT
+    email TEXT UNIQUE NOT NULL,
+    createdAt TEXT DEFAULT (datetime('now')),
+    modifiedAt TEXT DEFAULT (datetime('now'))
+  ) STRICT;
+
+  CREATE TRIGGER IF NOT EXISTS update_users_modifiedAt
+  BEFORE UPDATE ON users
+  FOR EACH ROW
+  BEGIN
+    UPDATE users
+    SET modifiedAT = datetime('now')
+    WHERE id = OLD.id;
+  END;
+
+  CREATE TRIGGER IF NOT EXISTS prevent_users_createdAt_modification
+  BEFORE UPDATE ON users
+  FOR EACH ROW
+  WHEN NEW.createdAt != OLD.createdAt
+  BEGIN
+    SELECT RAISE(ABORT, 'createdAt connot be modified');
+  END;
 `;
 
 db.exec(init);
