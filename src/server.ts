100c655da50347bfa1f427ed2fe4b7e0516ba301
proallone <proallone14@gmail.com>
2025-07-19 09:51:48 +0200

    add gracefull shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import app from "./app.js";
 import config from "./config/config.js";
+import db from "./database/database.js";
 
-app.listen(config.port, () => {
+const server = app.listen(config.port, () => {
     console.log(
         `Server running on port ${config.port} in ${config.nodeEnv} environment`
     );
 });
+
+const shutdown = () => {
+  console.log("\nShutting down...");
+  server.close(() => {
+    db.close();
+    console.log("Server closed. Database closed. Cleanup complete.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
