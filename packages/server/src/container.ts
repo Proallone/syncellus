867a3074190d8db78b175413bd734e0b275b229d
proallone <proallone14@gmail.com>
2025-08-26 19:46:52 +0200

    packages upgrade

--- packages/server/src/container.ts
+++ packages/server/src/container.ts
@@ -4,12 +4,17 @@ import { DatabaseService } from "./database/database.js";
 import { AuthRepository } from "@syncellus/modules/auth/repository.js";
 import { AuthService } from "@syncellus/modules/auth/service.js";
 import { LoggerService } from "@syncellus/core/logger.js";
+import { NodemailerMailer } from "@syncellus/modules/mailer/providers/NodemailerMailer.js";
+import { MailService } from "@syncellus/modules/mailer/service.js";
 
 const db = DatabaseService.getInstance();
 const logger = LoggerService.getInstance();
 
 const authRepo = new AuthRepository(db);
-const authService = new AuthService(authRepo);
+const mailer = new NodemailerMailer();
+const mailService = new MailService(mailer);
+
+const authService = new AuthService(authRepo, mailService);
 
 configurePassport(authService);
 
--- packages/server/src/utils/nanoid.ts
+++ packages/server/src/utils/nanoid.ts
@@ -1,3 +1,6 @@
 import { customAlphabet } from "nanoid";
 
-export const nanoid = customAlphabet("1234567890abcdef", 10);
+const alphabet = "1234567890abcdef"; //? we want simple, human readable ids
+const length = 10; //? not too long so user can memorize them
+
+export const nanoid = customAlphabet(alphabet, length);
