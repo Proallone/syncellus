808335894be3cbdb72f228a9fcfd1550f7fb6ea1
proallone <proallone14@gmail.com>
2025-08-31 21:12:19 +0200

    libsql migration

--- packages/server/src/database/database.ts
+++ packages/server/src/database/database.ts
@@ -1,7 +1,7 @@
 import type { Database as DB } from "@syncellus/types/database.js";
-import { Kysely, SqliteDialect } from "kysely";
-import Database from "better-sqlite3-multiple-ciphers";
-// import config from "@syncellus/configs/config.js";
+import { Kysely } from "kysely";
+import { LibsqlDialect } from "kysely-libsql";
+import { createClient } from "@libsql/client";
 export class DatabaseService {
     private static instance: Kysely<DB> | null = null;
 
@@ -12,21 +12,17 @@ export class DatabaseService {
             const encryptionKey = process.env.DATABASE_KEY; //TODO fix this to use config
 
             const isDev = ["dev", "development", "test"].includes(process.env.NODE_ENV); //TODO fix this to use config
+
             if (!encryptionKey && !isDev) {
                 throw new Error("DATABASE_KEY environment variable is not set.");
             }
 
-            const dbInstance = new Database("syncellus.sqlite");
-            if (!isDev) {
-                //encrypt database only if not in dev
-                dbInstance.pragma(`key='${encryptionKey}'`); //? this has to be executed first - otherwise throws
-            }
-
-            dbInstance.pragma(`journal_mode = WAL`);
-
-            const dialect = new SqliteDialect({
-                database: dbInstance
+            const client = createClient({
+                url: "file:syncellus.sqlite",
+                encryptionKey: !isDev ? encryptionKey : null
             });
+
+            const dialect = new LibsqlDialect({ client });
 
             DatabaseService.instance = new Kysely<DB>({ dialect });
         }
