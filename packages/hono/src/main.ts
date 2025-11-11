c352074027796a25ae16a75ecd17ca0e43e84c7a
Ołówek <proallone14@gmail.com>
2025-11-11 17:48:53 +0100

    add secureHeaders

--- packages/hono/src/main.ts
+++ packages/hono/src/main.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
+import { secureHeaders } from "hono/secure-headers";
 import { logger } from "hono/logger";
 import healthRouter from "@syncellus/hono/modules/health/routes.ts";
 import authRouter from "@syncellus/hono/modules/auth/routes.ts";
-
 const app = new Hono().basePath("/api/v1");
 
 app.use(logger());
+app.use(secureHeaders());
 app.route("/health", healthRouter);
 app.route("/auth", authRouter);
 
