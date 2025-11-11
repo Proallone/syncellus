e9257ee4e098848c905842039539f82e38c70f31
Ołówek <proallone14@gmail.com>
2025-11-11 21:38:17 +0100

    add simple global error handler

--- packages/hono/src/main.ts
+++ packages/hono/src/main.ts
@@ -4,6 +4,7 @@ import { logger } from "hono/logger";
 import healthRouter from "@syncellus/hono/modules/health/routes.ts";
 import authRouter from "@syncellus/hono/modules/auth/routes.ts";
 import { AppConfig } from "@syncellus/hono/config/config.ts";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono().basePath("/api/v1");
 
@@ -12,6 +13,15 @@ app.use(secureHeaders());
 
 app.route("/health", healthRouter);
 app.route("/auth", authRouter);
+
+app.onError((error, _c) => {
+	if (error instanceof HTTPException) {
+		console.error(error);
+		return error.getResponse(); //TODO make better
+	}
+
+	return new Response("An unexpected error occurred");
+});
 
 const port = AppConfig.getInstance().PORT;
 Deno.serve({ port: port }, app.fetch);
