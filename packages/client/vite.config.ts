86185e5b9fad6c5f2646b616836a93e90976c219
Ołówek <proallone14@gmail.com>
2025-11-22 10:56:39 +0100

    client packages upgrade

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { resolve } from "@std/path";
 import { defineConfig, loadEnv } from "vite";
+import deno from "@deno/vite-plugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, Deno.cwd(), "");
@@ -11,10 +12,13 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), deno(), tailwindcss()],
+    optimizeDeps: {
+      include: ["react/jsx-runtime"],
+    },
     resolve: {
       alias: {
-        "@": path.resolve(import.meta.dirname!, "./src"),
+        "@": resolve(import.meta.dirname!, "./src"),
       },
     },
     server: {
